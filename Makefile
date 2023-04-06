e84d5251e258cb901c1cd30c7a99d0cd91a5993e
KatzaCat <katzacat1@gmail.com>
2023-04-05 20:27:49 -0500

    a opening window

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: ofile
-	gcc *.o -o RayCaster -s -L E:\programing\lybraries\SDL\lib -lmingw32 -lSDL2main -lSDL2 -lgdi32
+	gcc *.o -o RayCaster -s -L E:\programing\lybraries\SDL2-2.26.5\lib -lmingw32 -lSDL2main -lSDL2 -lopengl32 -lgdi32
 
 ofile:
-	gcc -c ./*.c -Wall -I E:\programing\lybraries\SDL\include
+	gcc -c ray-caster.c -O3 -Wall -I E:\programing\lybraries\SDL2-2.26.5\include
--- ray-caster.c
+++ ray-caster.c
@@ -1,11 +1,61 @@
-#include <SDL2\SDL.h>
-#include <SDL2\SDL_image.h>
-
-#undef main
-
 #include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2\SDL.h>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define WINDOW_TITLE "RayCaster"
+
+// window ->
+
+struct Window {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    bool quit;
+};
+
+struct Window window;
+
+// <- window
+
+void init() {
+    // SDL initilization
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) > 0) {printf("INITALIZATION OF SDL VIDEO AND AUDIO FAILED WITH ERROR: %s", SDL_GetError()); window.quit = true;}
+    printf("INITALIZATION OF SDL VIDEO AND AUDIO SUCCESS");
+    
+    // window ->
+    window.window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    window.renderer = SDL_CreateRenderer(window.window, -1, 0);
+    // <- window
+}
+
+void update() {}
+
+void event() {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        // window ->
+        if(event.type == SDL_QUIT){window.quit = true;}
+        // <- window
+    }
+}
+
+void draw() {}
+
+void destroy() {
+    SDL_DestroyWindow(window.window);
+    SDL_DestroyRenderer(window.renderer);
+    SDL_Quit();
+} 
 
 int main(int argv, char *argc[]) {
-    printf("hello world");
+    init();
+    while (!window.quit) {
+        event();
+        update();
+        draw();
+    }
+    destroy();
     return 0;
 }
